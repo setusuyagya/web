ab72069355fd1fec2951b1bb55c9e8062de18f47
setusuyagya <38158897+setusuyagya@users.noreply.github.com>
2019-12-03 20:29:12 +0530

    web code

---
+++ web/p7.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1"/>
+<style>
+p
+{
+color:white;
+font-size:90px;
+position:absolute;
+top:50%;
+left:50%;
+tranform:transalte(-50%,-50%);
+}
+body{
+background-color:black;
+}
+</style>
+<p>
+	<?php echo date("h:i:s A"); 
+?> 
+</p>
+</head>
+</html>
+
+
+
+
